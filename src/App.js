d65390a64ed5e43510f253d373bde14ad506ff27
roger.eduardo@gmail.com <roger.eduardo@gmail.com>
2019-07-06 12:38:35 +1000

    Scaffold page

--- src/App.js
+++ src/App.js
@@ -5,20 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label for="create-task">task:</label>
+      <input type="text" id="create-task" />
+      <hr/>
+      <div id='actions'>
+        <ul>
+          <li>Action 1</li>
+          <li>Action 1</li>
+          <li>Action 1</li>
+        </ul>
+      </div>
     </div>
   );
 }
