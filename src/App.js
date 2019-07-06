c8f2bd32ab076f78210e1a6cffb96cc3822b0c54
roger.eduardo@gmail.com <roger.eduardo@gmail.com>
2019-07-06 12:58:47 +1000

    Split the tasks area into two areas: Todo and Done

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ function App() {
       <label htmlFor="create-task">task:</label>
       <input type="text" id="create-task" />
       <hr/>
-      <div id='actions'>
-        <ul>
-          <li>Action 1</li>
-          <li>Action 1</li>
-          <li>Action 1</li>
-        </ul>
+      <div className="row">
+        <div id="tasks-todo" className="col">
+          <h2>To do:</h2>
+          <ul>
+            <li>Action 1</li>
+            <li>Action 1</li>
+            <li>Action 1</li>
+          </ul>
+        </div>
+        <div id="tasks-done" className="col">
+          <h2>Done:</h2>
+          <ul>
+            <li>Action 1</li>
+            <li>Action 1</li>
+            <li>Action 1</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
