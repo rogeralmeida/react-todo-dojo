477b452551c753bb49ae7c92a12b62bb6dc42660
roger.eduardo@gmail.com <roger.eduardo@gmail.com>
2019-07-06 12:53:37 +1000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <label for="create-task">task:</label>
+    <div className="container">
+      <label htmlFor="create-task">task:</label>
       <input type="text" id="create-task" />
       <hr/>
       <div id='actions'>
